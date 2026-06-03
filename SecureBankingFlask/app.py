835f1aeb2c34563abeb28d533a1691ecb32d1344
Zaid-software <shaikabulzaid@outlook.com>
2026-06-03 09:06:37 +0700

    fix: changed port listening from 5000 to 5001

--- SecureBankingFlask/app.py
+++ SecureBankingFlask/app.py
@@ -68,4 +68,4 @@ def server_error(e):
 
 if __name__ == "__main__":
     # Never run debug=True in production
-    app.run(host="0.0.0.0", port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5001, debug=False)
